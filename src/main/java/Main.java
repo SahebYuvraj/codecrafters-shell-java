8349eed4d73f8f7a3c85c286d9a69aff00008d0a
SahebYuvraj <u7781798@anu.edu.au>
2025-12-06 07:05:28 +1100

    Completed part 4/43

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,15 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // REPL - read eval print loop
         while(true){
+
             // Display prompt
             System.out.print("$ ");
 
             // Read user input -- all inputs are treated as unknown commands
             Scanner scanner = new Scanner(System.in);
             String command = scanner.nextLine();
+            
+            if (command.equals("exit")) break;
             System.out.println(command+": command not found");
 
             
